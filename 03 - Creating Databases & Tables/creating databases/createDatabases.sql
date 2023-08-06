00ef425973917907e9286ad9b1fefc57a8fb1324
NunoGitHub <nunoaapp22@hotmail.com>
2023-08-06 20:11:41 +0100

    creation delete  databases

---
+++ 03 - Creating Databases & Tables/creating databases/createDatabases.sql
@@ -0,0 +1,16 @@
+SHOW databases;
+
+create database pet_shop;
+
+create database SlimeStore;
+
+create database chicken_coop;
+
+-- remove
+drop database SlimeStore;
+
+-- selecting working database
+use chicken_coop;
+
+select database()
+-- 3 / 8 
