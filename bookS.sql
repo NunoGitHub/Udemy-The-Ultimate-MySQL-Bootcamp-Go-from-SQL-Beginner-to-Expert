a7163dbe6341ac6adbbc749d3926d1eddb462d0b
NunoGitHub <nunoaapp22@hotmail.com>
2023-08-26 20:21:33 +0100

    substring

---
+++ bookS.sql
@@ -0,0 +1,71 @@
+CREATE DATABASE book_shop;
+
+USE book_shop;
+
+
+CREATE TABLE books 
+	(
+		book_id INT NOT NULL AUTO_INCREMENT,
+		title VARCHAR(100),
+		author_fname VARCHAR(100),
+		author_lname VARCHAR(100),
+		released_year INT,
+		stock_quantity INT,
+		pages INT,
+		PRIMARY KEY(book_id)
+	);
+
+INSERT INTO books (title, author_fname, author_lname, released_year, stock_quantity, pages)
+VALUES
+('The Namesake', 'Jhumpa', 'Lahiri', 2003, 32, 291),
+('Norse Mythology', 'Neil', 'Gaiman',2016, 43, 304),
+('American Gods', 'Neil', 'Gaiman', 2001, 12, 465),
+('Interpreter of Maladies', 'Jhumpa', 'Lahiri', 1996, 97, 198),
+('A Hologram for the King: A Novel', 'Dave', 'Eggers', 2012, 154, 352),
+('The Circle', 'Dave', 'Eggers', 2013, 26, 504),
+('The Amazing Adventures of Kavalier & Clay', 'Michael', 'Chabon', 2000, 68, 634),
+('Just Kids', 'Patti', 'Smith', 2010, 55, 304),
+('A Heartbreaking Work of Staggering Genius', 'Dave', 'Eggers', 2001, 104, 437),
+('Coraline', 'Neil', 'Gaiman', 2003, 100, 208),
+('What We Talk About When We Talk About Love: Stories', 'Raymond', 'Carver', 1981, 23, 176),
+("Where I'm Calling From: Selected Stories", 'Raymond', 'Carver', 1989, 12, 526),
+('White Noise', 'Don', 'DeLillo', 1985, 49, 320),
+('Cannery Row', 'John', 'Steinbeck', 1945, 95, 181),
+('Oblivion: Stories', 'David', 'Foster Wallace', 2004, 172, 329),
+('Consider the Lobster', 'David', 'Foster Wallace', 2005, 92, 343);
+
+SELECT * FROM books
+
+DESC books
+
+-- string functions 
+
+SELECT CONCAT('h', 'e', 'l')
+
+SELECT 'hel'
+
+-- strings
+
+
+SELECT CONCAT(books.author_fname,' ' ,books.author_lname) as fullName FROM books
+
+-- with seperator
+SELECT CONCAT_WS(' ', books.author_fname, books.author_lname) as FullName FROM books
+
+SELECT CONCAT_WS('-', title, author_fname, author_lname) as info FROM books
+
+-- substring
+SELECT SUBSTRING('hello world', 1,5)
+
+SELECT SUBSTRING('hello world', 7)
+
+SELECT SUBSTRING('hello world', -3)
+
+
+SELECT SUBSTRING(title,1,15) as title FROM books
+
+
+SELECT SUBSTR(title, 1, 15) as title FROM books
+
+
+SELECT SUBSTRING(author_lname, 1,1) as initial , author_lname FROM books
